77b209ce4f0de7814eb10943808760c8bd3254ad
egdose <i180504@nu.edu.pk>
2021-03-24 00:19:57 +0500

    Fifth Commit
    Working Increment
    
    -BFS Implemented
    -Fixed an issue with the graph top_nodes
    -Implemented image generation from visited array after algorithm finishes.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import pandas as pd
+from PIL import Image
 
 #Important Enum variables for Indexing
 UP = 0
@@ -14,8 +15,26 @@ class Maze:
 			self.Adjacents = [None, None, None, None]
 			self.x = x
 			self.y = y
-			self.cost = [0, 0, 0, 0]
+			#self.cost = [0, 0, 0, 0]
 			#print(f"New Node({self.x}, {self.y})")
+
+		def print_info(self):
+			if self.Adjacents[UP] != None:
+				print(f"->Up ({self.Adjacents[UP].x}, {self.Adjacents[UP].y})")
+			else:
+				print(f"->Up (None)")
+			if self.Adjacents[LEFT] != None:
+				print(f"->Left ({self.Adjacents[LEFT].x}, {self.Adjacents[LEFT].y})")
+			else:
+				print(f"->Left (None)")
+			if self.Adjacents[RIGHT] != None:
+				print(f"->Right ({self.Adjacents[RIGHT].x}, {self.Adjacents[RIGHT].y})")
+			else:
+				print(f"->Right (None)")
+			if self.Adjacents[DOWN] != None:
+				print(f"->Down ({self.Adjacents[DOWN].x}, {self.Adjacents[DOWN].y})")
+			else:
+				print(f"->Down (None)")
 
 
 
@@ -120,7 +139,8 @@ class Maze:
 				#Using Top Buffer
 				if new_node != None:
 					#If there exists a node above
-					if temp_top[x] != None:
+					#if temp_top[x] != None: #BAD BAD IF
+					if self.grid[new_node.y-1, new_node.x]:
 						temp_top[x].Adjacents[DOWN] = new_node
 						new_node.Adjacents[UP] = temp_top[x]
 
@@ -157,11 +177,12 @@ class Maze:
 
 		self.traverse_util(self.start_node, temp_count, temp_grid)
 
-		print("Counted Nodes:", temp_count[0])
+		print("Confirmed Nodes:", temp_count[0])
 
 
 	def traverse_util(self, cur, temp_count, temp_grid):
-		#print(f"Node Traversed at({cur.x}, {cur.y})")
+		print(f"Node Traversed at({cur.x}, {cur.y})")
+		cur.print_info()
 		temp_count[0] += 1
 		temp_grid[cur.y, cur.x] = False
 		up_node = cur.Adjacents[UP]
@@ -198,6 +219,9 @@ class Maze:
 			print(f"Algorithm used = \"DFS\", No of moves utilized = {temp_count[0]}")
 		else:
 			print(f"Algorithm used = \"DFS\", End State Not Found!")
+
+		
+		self.save_image("DFS", temp_grid)
 
 
 	def DFS_util(self, cur, temp_count, temp_grid):
@@ -236,8 +260,74 @@ class Maze:
 
 		return False
 
+	def BFS(self):
+		temp_count = np.zeros(1, dtype=int)
+		temp_grid = np.copy(self.grid)
+
+		if self.start_node == None:
+			print("Start Node shouldn't be None!")
+			return
+		elif self.end_node == None:
+			print("End Node shouldn't be None!")
+			return
+
+		queue = []
+
+		queue.append(self.start_node)
+		temp_grid[self.start_node.y, self.start_node.x] = False
+
+		is_finished = False
+
+		while(len(queue) != 0):
+			#Updating Current Node
+			cur = queue.pop(0)
+			#Printing/Debugging
+			#print(f"Node Traversed at({cur.x}, {cur.y})")
+
+			#Incrementing the Cost
+			temp_count[0] += 1
+			if cur == self.end_node:
+				is_finished = True
+				break
+			#Some temporary variables
+			up_node = cur.Adjacents[UP]
+			left_node = cur.Adjacents[LEFT]
+			right_node = cur.Adjacents[RIGHT]
+			down_node = cur.Adjacents[DOWN]
+			#Inserting to the queue
+			if up_node != None and temp_grid[up_node.y, up_node.x]:
+				queue.append(up_node)
+				temp_grid[up_node.y, up_node.x] = False
+			if left_node != None and temp_grid[left_node.y, left_node.x]:
+				queue.append(left_node)
+				temp_grid[left_node.y, left_node.x] = False
+			if right_node != None and temp_grid[right_node.y, right_node.x]:
+				queue.append(right_node)
+				temp_grid[right_node.y, right_node.x] = False
+			if down_node != None and temp_grid[down_node.y, down_node.x]:
+				queue.append(down_node)
+				temp_grid[down_node.y, down_node.x] = False
+			
+		if is_finished:
+			print(f"Algorithm used = \"BFS\", No of moves utilized = {temp_count[0]}")
+		else:
+			print(f"Algorithm used = \"BFS\", End State Not Found!")
 
 
+		self.save_image("BFS", temp_grid)
+
+
+	def save_image(self, algo_name, temp_grid):
+		image_grid = np.copy(self.grid).astype('uint8')*255
+
+		for x in range(self.width):
+			for y in range(self.height):
+				if self.grid[y, x]:
+					if temp_grid[y, x] == False:
+						image_grid[y, x] = 127
+
+		im = Image.fromarray(image_grid, 'L')
+		im.resize((512,512), Image.NEAREST).save(algo_name+".png")
 
 
 def main():
@@ -267,10 +357,11 @@ def main():
 	maze = Maze(maze_dimensions)
 
 	#Checking Nodes
-	maze.traverse_graph()
+	#maze.traverse_graph()
 
 	#Find Result
 	maze.DFS()
+	maze.BFS()
 
 
 if __name__ == '__main__':
